a4a69e04b165586426abea320d7abbc8058ed341
Kevin Firko <kfirko@bitcurve.com>
2022-06-18 16:22:04 -0400

    initial commit

---
+++ apps/fx-nx-prisma-stack-e2e/src/integration/app.spec.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('fx-nx-prisma-stack', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome fx-nx-prisma-stack');
+  });
+});
