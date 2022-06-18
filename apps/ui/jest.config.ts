b381450412c0add733b6698f6c6d66828cb19634
Kevin Firko <kfirko@bitcurve.com>
2022-06-18 16:39:41 -0400

    spin up nx project with react ui (default) and nest api apps

--- apps/ui/jest.config.ts
+++ apps/ui/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'fx-nx-prisma-stack',
+  displayName: 'ui',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/fx-nx-prisma-stack',
+  coverageDirectory: '../../coverage/apps/ui',
 };
