958b301fd5706c7bee5cec063ee178cdb1df3a59
Kevin Firko <kfirko@bitcurve.com>
2022-07-19 02:44:13 -0400

    ui add react ErrorBoundary to app

--- apps/ui/src/pages/_app.tsx
+++ apps/ui/src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { SWRConfig } from 'swr' // useSWR
+
 import { SessionContextProvider } from '../context/SessionContextProvider'
 import { ProtectedLayout } from '../components/layout/ProtectedLayout'
 import { PublicLayout } from '../components/layout/PublicLayout'
 import { SessionLoadingScreen } from '../components/layout/SessionLoadingScreen'
+import { ErrorBoundary } from '../components/layout/ErrorBoundary'
 
 import '../styles/tailwind.css'
 
@@ -15,35 +17,39 @@ const PUBLIC_ROUTES = ['/', '/sign-in']
 const isPublicRoute = (routerPath: string) =>
   PUBLIC_ROUTES.some((route) => routerPath === '/' || routerPath.startsWith(route))
 
-function CustomApp({ Component, pageProps, router }: AppProps) {
+function CustomApp(props: AppProps) {
+  const { Component, pageProps, router } = props
+
   return (
     <>
       <Head>
         <title>UI</title>
       </Head>
-      <SWRConfig
-        value={{
-          refreshInterval: 3000,
-          fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()),
-        }}
-      >
-        <SessionContextProvider>
-          {(isSessionReady) =>
-            isPublicRoute(router.asPath) ? (
-              <PublicLayout>
-                <Component {...pageProps} />
-              </PublicLayout>
-            ) : isSessionReady ? (
-              <ProtectedLayout>
-                {/* autherrorlistener, sessiontimer, etc */}
-                <Component {...pageProps} />
-              </ProtectedLayout>
-            ) : (
-              <SessionLoadingScreen />
-            )
-          }
-        </SessionContextProvider>
-      </SWRConfig>
+      <ErrorBoundary>
+        <SWRConfig
+          value={{
+            refreshInterval: 3000,
+            fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()),
+          }}
+        >
+          <SessionContextProvider>
+            {(isSessionReady) =>
+              isPublicRoute(router.asPath) ? (
+                <PublicLayout>
+                  <Component {...pageProps} />
+                </PublicLayout>
+              ) : isSessionReady ? (
+                <ProtectedLayout>
+                  {/* autherrorlistener, sessiontimer, etc */}
+                  <Component {...pageProps} />
+                </ProtectedLayout>
+              ) : (
+                <SessionLoadingScreen />
+              )
+            }
+          </SessionContextProvider>
+        </SWRConfig>
+      </ErrorBoundary>
     </>
   )
 }
