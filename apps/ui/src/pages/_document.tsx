57191d16b94fc0cd6b343c918f42f2538452b30e
Kevin Firko <kfirko@bitcurve.com>
2022-07-21 19:50:56 -0400

    ui add custom nextjs _document

---
+++ apps/ui/src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default CustomDocument
