2bf8af5b241284508432788bf24df5ecbe34b51d
Kevin Firko <kfirko@bitcurve.com>
2022-07-21 20:01:54 -0400

    ui remove swr config from _app and add in react-query QueryClientProvider

--- apps/ui/src/pages/_app.tsx
+++ apps/ui/src/pages/_app.tsx
@@ -1,54 +1,61 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { SWRConfig } from 'swr' // useSWR
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+import '../styles/tailwind.css'
+import { ErrorBoundary } from '../components/layout/ErrorBoundary'
 import { SessionContextProvider } from '../context/SessionContextProvider'
 import { ProtectedLayout } from '../components/layout/ProtectedLayout'
 import { PublicLayout } from '../components/layout/PublicLayout'
 import { SessionLoadingScreen } from '../components/layout/SessionLoadingScreen'
-import { ErrorBoundary } from '../components/layout/ErrorBoundary'
 
-import '../styles/tailwind.css'
-
-// const fetcher = (resource, init) => fetch(resource, init).then((res) => res.json())
+import { AppLayout } from '../components/layout/AppLayout'
+import { useState } from 'react'
 
 const PUBLIC_ROUTES = ['/', '/sign-in']
+const NAVIGATION_LINKS = [
+  { title: 'App', href: '/app' },
+  { title: 'Secret', href: '/secret' },
+  { title: 'Sign-In', href: '/sign-in' },
+]
 
 const isPublicRoute = (routerPath: string) =>
-  PUBLIC_ROUTES.some((route) => routerPath === '/' || routerPath.startsWith(route))
+  routerPath === '/' ? true : PUBLIC_ROUTES.some((route) => (route === '/' ? false : routerPath.startsWith(route)))
 
-function CustomApp(props: AppProps) {
-  const { Component, pageProps, router } = props
+// @todo - some setting in vscode/nx/prettier/? keeps _intermittently_ removing the `AppProps` type - find and fix!
+
+function CustomApp({ Component, pageProps, router }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
 
   return (
     <>
       <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Web Application" key="description" />
         <title>UI</title>
       </Head>
       <ErrorBoundary>
-        <SWRConfig
-          value={{
-            refreshInterval: 3000,
-            fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()),
-          }}
-        >
+        <QueryClientProvider client={queryClient}>
           <SessionContextProvider>
-            {(isSessionReady) =>
-              isPublicRoute(router.asPath) ? (
-                <PublicLayout>
-                  <Component {...pageProps} />
-                </PublicLayout>
-              ) : isSessionReady ? (
-                <ProtectedLayout>
-                  {/* autherrorlistener, sessiontimer, etc */}
-                  <Component {...pageProps} />
-                </ProtectedLayout>
-              ) : (
-                <SessionLoadingScreen />
-              )
-            }
+            {(isSessionReady) => (
+              <AppLayout navigationLinks={NAVIGATION_LINKS}>
+                {isPublicRoute(router.asPath) ? (
+                  <PublicLayout>
+                    <Component {...pageProps} />
+                  </PublicLayout>
+                ) : isSessionReady ? (
+                  <ProtectedLayout>
+                    {/* autherrorlistener, sessiontimer, etc */}
+                    <Component {...pageProps} />
+                  </ProtectedLayout>
+                ) : (
+                  <SessionLoadingScreen />
+                )}
+              </AppLayout>
+            )}
           </SessionContextProvider>
-        </SWRConfig>
+        </QueryClientProvider>
       </ErrorBoundary>
     </>
   )
