0dc2689bb6ceae237f101c45f4c54e085c2d3d0c
Kevin Firko <kfirko@bitcurve.com>
2022-07-29 16:33:31 -0400

    ui refactor layout + loading screen components per SessionContextProvider changes

--- apps/ui/src/components/layout/SessionLoadingScreen.tsx
+++ apps/ui/src/components/layout/SessionLoadingScreen.tsx
@@ -1,21 +1,24 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
-import { useSession } from '../../context/SessionContextProvider'
+import { useSessionContext } from '../../context/SessionContextProvider'
 import { Spinner } from '../elements/Spinner'
 
 export const SessionLoadingScreen = () => {
   const { push: routerPush } = useRouter()
-  const session = useSession()
+  const session = useSessionContext()
+
+  const userSession = session?.session
+  const userSessionError = session?.error
 
   // delayed redirect is used to support the back-button
   // this approach does not clobber the user's navigation history vs. router.replace()
   useEffect(() => {
-    if (!session.session && session.error) {
+    if (!userSession && userSessionError) {
       const timeoutId = setTimeout(() => routerPush('/sign-in'), 300)
       return () => clearTimeout(timeoutId)
     }
-  }, [session.session, session.error, routerPush])
+  }, [userSession, userSessionError, routerPush])
 
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-slate-100">
