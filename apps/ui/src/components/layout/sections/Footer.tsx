cf7a1c0583e50e974d3847d136c7efd64489c30e
Kevin Firko <kfirko@bitcurve.com>
2022-08-30 15:12:47 -0400

    ui add PlaceholderLayout component w/ corresponding PlaceholderHeader

--- apps/ui/src/components/layout/sections/Footer.tsx
+++ apps/ui/src/components/layout/sections/Footer.tsx
@@ -3,19 +3,19 @@ import Link from 'next/link'
 
 export interface FooterProps {}
 
+const footerClassName = clsx(
+  'flex flex-col sm:flex-row items-center justify-center w-full mx-auto text-center py-4',
+  'text-xs font-normal leading-none text-brand-slate-800',
+  'fx-layout-max-width fx-layout-padding-x',
+)
+
 /**
  * Footer for structure layout.
  */
 export const Footer: React.FC<FooterProps> = () => {
   return (
     <footer className="border-t-2 bg-slate-100 border-slate-200">
-      <div
-        className={clsx(
-          'flex flex-col sm:flex-row items-center justify-center w-full mx-auto text-center py-4',
-          'text-xs font-normal leading-none text-brand-slate-800',
-          'fx-layout-max-width fx-layout-padding-x',
-        )}
-      >
+      <div className={footerClassName}>
         {process.env.NEXT_PUBLIC_PROJECT_ORG_CONTACT_URL && process.env.NEXT_PUBLIC_PROJECT_ORG_NAME && (
           <div>
             <span>&copy; {new Date().getFullYear()} </span>
--- apps/ui/src/components/layout/sections/Header.tsx
+++ apps/ui/src/components/layout/sections/Header.tsx
@@ -177,21 +177,43 @@ const MobileNavMenu: React.FC<
   )
 }
 
+const headerClassName = 'relative border-b-2 bg-slate-100 border-slate-200'
+
+const navClassName = clsx(
+  'relative mx-auto flex items-center justify-between py-3',
+  'fx-layout-max-width fx-layout-padding-x',
+)
+
+/**
+ * Non-dynamic placeholder header that corresponds to `Header` component.
+ *
+ * @see Header
+ */
+export const PlaceholderHeader: React.FC = () => {
+  return (
+    <header className={headerClassName}>
+      <nav className={navClassName} aria-label="Main">
+        <div className="flex items-center flex-1">
+          <div className="flex items-center justify-between w-full lg:w-auto">
+            <div className="flex items-center space-x-4">
+              <LogoLink />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
 /**
  * Header with branding that implements a responsive navigation menu.
  */
 export const Header: React.FC<HeaderProps> = ({ navigationLinks }) => {
   return (
-    <Popover as="header" className={clsx('relative border-b-2 bg-slate-100 border-slate-200')}>
+    <Popover as="header" className={headerClassName}>
       {({ open, close }) => (
         <>
-          <nav
-            className={clsx(
-              'relative mx-auto flex items-center justify-between py-3',
-              'fx-layout-max-width fx-layout-padding-x',
-            )}
-            aria-label="Main"
-          >
+          <nav className={navClassName} aria-label="Main">
             <div className="flex items-center flex-1">
               <div className="flex items-center justify-between w-full lg:w-auto">
                 <div className="flex items-center space-x-4">
