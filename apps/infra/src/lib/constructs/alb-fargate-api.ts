83f590bc7ad1377db594b933f3f3529ca11e4be3
Kevin Firko <kfirko@bitcurve.com>
2022-09-06 14:21:06 -0400

    infra iterate AlbFargateApi construct for project stack + add logs + healthcheck props - will deploy successfully

--- apps/infra/src/lib/constructs/alb-fargate-api.ts
+++ apps/infra/src/lib/constructs/alb-fargate-api.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
-import { CfnOutput, Duration } from 'aws-cdk-lib'
+import { CfnOutput, Duration, RemovalPolicy } from 'aws-cdk-lib'
 
 import * as iam from 'aws-cdk-lib/aws-iam'
+import * as logs from 'aws-cdk-lib/aws-logs'
 import * as route53 from 'aws-cdk-lib/aws-route53'
 import * as acm from 'aws-cdk-lib/aws-certificatemanager'
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
@@ -14,16 +15,18 @@ import { FxBaseConstruct, FxBaseConstructProps } from '../abstract/fx-base.abstr
 import { FxBaseStack } from '../abstract/fx-base.abstract.stack'
 
 export interface AlbFargateApiProps extends FxBaseConstructProps {
-  cluster: ecs.ICluster
   ecrRepository: ecr.IRepository
   ecs: {
+    cluster: ecs.ICluster
     container: {
+      name?: string
       environment?: Record<string, string>
       secrets?: Record<string, ecs.Secret>
       port: number
       executionRole?: iam.Role
     }
     service?: {
+      name?: string
       securityGroups?: ec2.ISecurityGroup[]
     }
     task?: {
@@ -31,6 +34,8 @@ export interface AlbFargateApiProps extends FxBaseConstructProps {
       memoryLimit?: ecsPatterns.ApplicationLoadBalancedFargateServiceProps['memoryLimitMiB']
       cpuLimit?: ecsPatterns.ApplicationLoadBalancedFargateServiceProps['cpu']
       desiredCount?: number
+      minHealthyPercent?: number
+      maxHealthyPercent?: number
     }
     logs?: {
       logDriver: ecs.LogDriver
@@ -48,9 +53,31 @@ export interface AlbFargateApiProps extends FxBaseConstructProps {
     }
   }
 
+  alb: {
+    publicLoadBalancer: boolean
+    assignPublicIp: boolean
+
+    targetGroup?: {
+      healthcheck?: {
+        path: string
+        healthyHttpCodes: string
+      }
+    }
+  }
+
   api: {
+    /**
+     * Base path of the API per project convention (e.g. `/{PROJECT_TAG}/api`) that forms part of the complete
+     * API URI that the containerized API is listenening at (`{BASE_PATH}/{VERSION}` e.g. `project-tag/api/v1`)
+     * and associate with the containerized API running on Fargate.
+     */
     basePath: string
-    uriVersion: string
+
+    /**
+     * API version prefixed with a 'v' e.g. 'v1', 'v2', etc that forms part of the complete API URI
+     * (`{BASE_PATH}/{VERSION}` e.g. `project-tag/api/v1`).
+     */
+    version: string
   }
 
   zone: route53.IHostedZone
@@ -58,6 +85,11 @@ export interface AlbFargateApiProps extends FxBaseConstructProps {
   domainName: string
 }
 
+/**
+ * Construct to launch a containerized API as an AWS Fargate service behind an Application Load Balancer (ALB).
+ *
+ * The API must be configured to adhere to project convention
+ */
 export class AlbFargateApi extends FxBaseConstruct {
   readonly albfs: ecsPatterns.ApplicationLoadBalancedFargateService
 
@@ -72,20 +104,35 @@ export class AlbFargateApi extends FxBaseConstruct {
 
     // reminder: be careful about modifying albfs values on production deployments to avoid hangs
     this.albfs = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'ALBFS', {
-      serviceName: `${parent.getProjectTag()}-${parent.getDeployStageTag()}`,
-      cluster: props.cluster,
-      publicLoadBalancer: true,
-      assignPublicIp: false,
-      // taskSubnets: ...
-      // taskDefinition: ... // alternative to specifying taskImageOptions
+      serviceName: props.ecs.service?.name ?? `${parent.getProjectTag()}-${parent.getDeployStageTag()}`,
+      cluster: props.ecs.cluster,
+      securityGroups: props.ecs.service?.securityGroups,
+
+      publicLoadBalancer: props.alb.publicLoadBalancer,
+      assignPublicIp: props.alb.assignPublicIp,
+      // taskSubnets: ... // @future could make private alb and put http-api up front w/ vpclink (for low-mid scale w/ private alb)
+
+      // taskDefinition: ... // alternative to specifying taskImageOptions is to define them separately
       taskImageOptions: {
-        containerName: `${parent.getProjectTag()}-${parent.getDeployStageTag()}-api`,
+        containerName: props.ecs.container.name ?? `${parent.getProjectTag()}-${parent.getDeployStageTag()}-api`,
         image: ecs.ContainerImage.fromEcrRepository(props.ecrRepository),
         containerPort: props.ecs.container.port,
+
         environment: props.ecs.container.environment,
         secrets: props.ecs.container.secrets,
+
         enableLogging: true,
-        logDriver: props.ecs.logs?.logDriver,
+        logDriver:
+          props.ecs.logs?.logDriver ??
+          ecs.LogDriver.awsLogs({
+            streamPrefix: `${parent.getProjectTag()}-${parent.getDeployStageTag()}`,
+            logGroup: new logs.LogGroup(this, 'LogGroup', {
+              logGroupName: `/aws/ecs/${parent.getProjectTag()}/api/${parent.getDeployStageTag()}`,
+              retention: parent.isProduction() ? logs.RetentionDays.ONE_YEAR : logs.RetentionDays.THREE_DAYS,
+              removalPolicy: RemovalPolicy.DESTROY,
+            }),
+          }),
+
         taskRole: props.ecs.task?.taskRole,
         executionRole: props.ecs.container.executionRole,
       },
@@ -104,11 +151,10 @@ export class AlbFargateApi extends FxBaseConstruct {
       },
 
       propagateTags: ecs.PropagatedTagSource.SERVICE,
-      securityGroups: props.ecs.service?.securityGroups,
-      desiredCount: props.ecs.task?.desiredCount,
 
-      // maxHealthyPercent: 200,
-      // minHealthyPercent: 50,
+      desiredCount: props.ecs.task?.desiredCount,
+      minHealthyPercent: props.ecs.task?.minHealthyPercent ?? 50,
+      maxHealthyPercent: props.ecs.task?.maxHealthyPercent ?? 200,
     })
 
     this.scaling = {
@@ -124,10 +170,13 @@ export class AlbFargateApi extends FxBaseConstruct {
       scaleOutCooldown: props.ecs.scaling?.taskCpuThreshold?.scaleOutCooldown ?? Duration.seconds(120),
     })
 
-    this.albfs.targetGroup.configureHealthCheck({
-      path: `${props.api.basePath}/${props.api.uriVersion}`, // leading slash required
-      healthyHttpCodes: '200-299',
-    })
+    // EC2 Target groups
+    if (props.alb.targetGroup?.healthcheck) {
+      this.albfs.targetGroup.configureHealthCheck({
+        path: props.alb.targetGroup?.healthcheck.path, // `${props.api.basePath}/${props.api.version}`, // leading slash required
+        healthyHttpCodes: props.alb.targetGroup?.healthcheck.healthyHttpCodes, // '200-299',
+      })
+    }
 
     this.printOutputs()
   }
@@ -140,9 +189,20 @@ export class AlbFargateApi extends FxBaseConstruct {
   }
 
   private assertValidProps(props: AlbFargateApiProps): true {
+    const errorPrefix = 'Validation Error:'
+
     if (props.api.basePath.slice(0, 1) !== '/') {
-      throw new Error(`API basePath value must have a leading forward slash. Received: '${props.api.basePath}'`)
+      throw new Error(
+        `${errorPrefix} API basePath value must have a leading forward slash. Received: '${props.api.basePath}'`,
+      )
     }
+
+    if (!/v\d+/.test(props.api.version)) {
+      throw new Error(
+        `${errorPrefix} API version value must match /v\d+/ e.g. 'v1', 'v2', etc. Received: '${props.api.version}'`,
+      )
+    }
+
     return true
   }
 }
