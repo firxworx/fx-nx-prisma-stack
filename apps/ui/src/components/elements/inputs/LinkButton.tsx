f99a5f187ff7eaf56daef7ff94136d6e06f54f3b
Kevin Firko <kfirko@bitcurve.com>
2022-09-09 22:31:12 -0400

    ui add ButtonSharedProps and revise all 3x buttons to use + ensure style/feature parity as applicable

--- apps/ui/src/components/elements/inputs/LinkButton.tsx
+++ apps/ui/src/components/elements/inputs/LinkButton.tsx
@@ -2,46 +2,63 @@ import React, { type PropsWithChildren } from 'react'
 import Link, { type LinkProps } from 'next/link'
 import clsx from 'clsx'
 
-export interface LinkButtonProps extends LinkProps {
-  href: string
-  variant?: 'solid' | 'outline' | 'transparent'
-  disabled?: boolean
-  appendClassName?: string
-  // anchorProps: Exclude<React.HTMLAttributes<HTMLAnchorElement>, 'className'>
-}
+import { Spinner } from '../feedback/Spinner'
+import type { ButtonSharedProps } from '../../../types/components/ButtonSharedProps.interface'
+
+export interface LinkButtonProps extends ButtonSharedProps, LinkProps {}
 
 /**
  * NextJS Link component styled like a button.
  *
  * The `appendClassName` prop supports adding additional style classes. The value is appended to `className`
  * after the component's built-in classes. It can be useful for adding classes for margins/spacing.
+ *
+ * @see FormButton for a button component integrated with react-hook-form.
+ * @see ActionButton for a standalone button that can be used outside the context of a form.
  */
 export const LinkButton: React.FC<PropsWithChildren<LinkButtonProps>> = ({
   href,
   variant,
   disabled,
   appendClassName,
+  isLoading,
+  isSubmitting,
   children,
   ...restProps
 }) => {
+  const renderDisabled = !!disabled || isLoading
+
   return (
     <Link href={href} {...restProps}>
       <a
         className={clsx(
           'fx-button-base',
           {
-            ['pointer-events-none']: disabled,
-            ['fx-button-solid-primary']: variant === 'solid' && !disabled,
-            ['fx-button-solid-primary-disabled']: variant === 'solid' && disabled,
-            ['fx-button-outline-primary']: variant === 'outline' && !disabled,
-            ['fx-button-outline-primary-disabled']: variant === 'outline' && disabled,
-            ['fx-button-transparent-primary']: variant === 'transparent' && !disabled,
-            ['fx-button-transparent-primary-disabled']: variant === 'transparent' && disabled,
+            // implement disabled with pointer events none at the expense of no custom cursor
+            ['pointer-events-none']: renderDisabled,
+
+            // conditional animation
+            'animate-pulse': isLoading || isSubmitting,
+
+            // button variant styles
+            ['fx-button-solid-primary']: variant === 'solid' && !renderDisabled,
+            ['fx-button-solid-primary-disabled']: variant === 'solid' && renderDisabled,
+            ['fx-button-outline-primary']: variant === 'outline' && !renderDisabled,
+            ['fx-button-outline-primary-disabled']: variant === 'outline' && renderDisabled,
+            ['fx-button-transparent-primary']: variant === 'transparent' && !renderDisabled,
+            ['fx-button-transparent-primary-disabled']: variant === 'transparent' && renderDisabled,
           },
           appendClassName,
         )}
       >
-        {children}
+        {isLoading || isSubmitting ? (
+          <>
+            <Spinner size="sm" appendClassName="mr-1" />
+            <div className="inline-flex items-center justify-center">{children}</div>
+          </>
+        ) : (
+          <>{children}</>
+        )}
       </a>
     </Link>
   )
---
+++ apps/ui/src/types/components/ButtonSharedProps.interface.ts
@@ -0,0 +1,41 @@
+/**
+ * Common base button props for different types of buttons in this project's UI.
+ */
+export interface ButtonSharedProps {
+  /**
+   * Button styles for different UX cases. Default variant is solid.
+   */
+  variant?: 'solid' | 'outline' | 'transparent'
+
+  /**
+   * Control if the button click/submit behavior is disabled or not.
+   */
+  disabled?: boolean
+
+  /**
+   * Append additional css class names after the component's own className for positioning and additional
+   * customization. This prop can be useful for setting margins/spacing classNames.
+   */
+  appendClassName?: string
+
+  /**
+   * Conditionally render the button in a loading/submitting state with the button disabled and an animated spinner
+   * inside the button, prepended to the left of the button's children.
+   *
+   * This prop/feature may be used in place of setting `disabled` and implementing conditional child
+   * content during a loading state.
+   */
+  isLoading?: boolean
+
+  /**
+   * Conditionally render the button in a submitting/loading state with the button disabled and an animated spinner
+   * inside the button, prepended to the left of the button's children.
+   *
+   * This prop/feature may be used in place of setting `disabled` and implementing conditional child
+   * content during a loading state.
+   *
+   * This prop/feature is not available for `FormButton`'s as integrated with react-hook-form because this state
+   * value is obtained from the form library.
+   */
+  isSubmitting?: boolean
+}
