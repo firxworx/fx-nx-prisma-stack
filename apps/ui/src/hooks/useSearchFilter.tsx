35531d72c2e7614987011fce61b8a4769002d72f
Kevin Firko <kfirko@bitcurve.com>
2022-10-03 12:15:00 -0400

    ui add lodash debounce to useSearchFilter hook

--- apps/ui/src/hooks/useSearchFilter.tsx
+++ apps/ui/src/hooks/useSearchFilter.tsx
@@ -1,53 +1,54 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { debounce } from 'lodash'
 
-export type SearchFilterHookReturnValue<T = object> = [
-  search: string,
-  results: T[],
+export type UseSearchFilterReturnValue<T = object> = [
   handleSearchInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  results: T[],
+  searchInputRef: React.MutableRefObject<HTMLInputElement | undefined>,
 ]
 
 /**
- * @todo useSearchFilter input debounce
- * @todo consider adding sort capabilities within hook
+ * React hook that implements a basic debounced keyword search filter for arrays of objects.
+ *
+ * The given `items` are assumed to be a stable reference if the array's contents do not change.
+ *
+ * @param key values of this property will be searched for substring matches
+ * @param items array of like objects that contain the property `key`
+ * @returns returns a tuple containing: onChange handler, results array, ref to apply to input.
  */
-export function useSearchFilter<T extends object>(key: keyof T, items: T[]): SearchFilterHookReturnValue<T> {
-  const isMountedRef = useRef<boolean>(false)
-  const [searchTerm, setSearchTerm] = useState('')
+export function useSearchFilter<T extends object>(key: keyof T, items: T[]): UseSearchFilterReturnValue<T> {
   const [results, setResults] = useState<T[]>(items)
+  const searchInputRef = useRef<HTMLInputElement>()
 
-  useEffect(() => {
-    isMountedRef.current = true
-
-    return () => {
-      isMountedRef.current = false
-    }
-  }, [])
-
-  // reset results if value of items changes
-  useEffect(() => {
-    if (isMountedRef.current) {
-      setResults(items)
-    }
-  }, [items])
-
-  const handleSearchInputChange = useCallback(
-    function (event: React.ChangeEvent<HTMLInputElement>): void {
-      if (!isMountedRef.current) {
-        return
-      }
-
-      const value = event.target.value
-      setSearchTerm(value)
-
-      if (value.length === 0) {
+  // stable debounce search filter function
+  const debouncedSearch = useRef(
+    debounce((term: string, items: T[], key: keyof T) => {
+      if (term === '') {
         setResults(items)
-        return
       }
 
-      setResults(items.filter((item) => String(item[key]).toLowerCase().includes(value.toLowerCase())))
-    },
-    [items, key],
-  )
+      setResults(items.filter((item) => String(item[key]).toLowerCase().includes(term.toLowerCase())))
+    }, 250),
+  ).current
 
-  return [searchTerm, results, handleSearchInputChange]
+  // handle case where items and/or key change
+  useEffect(() => {
+    setResults(items)
+
+    if (searchInputRef.current) {
+      debouncedSearch(searchInputRef.current.value, items, key)
+    }
+  }, [items, key, debouncedSearch])
+
+  useEffect(() => {
+    return () => {
+      debouncedSearch.cancel()
+    }
+  }, [debouncedSearch])
+
+  const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    debouncedSearch(event.target.value, items, key)
+  }
+
+  return [handleSearchInputChange, results, searchInputRef]
 }
