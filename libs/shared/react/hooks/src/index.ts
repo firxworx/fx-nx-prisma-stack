2a4501beec44156874100ad51dd6e97a4ddc4ce0
Kevin Firko <kfirko@bitcurve.com>
2022-10-06 14:45:10 -0400

    react-hooks ui lib commit index

--- libs/shared/react/hooks/src/index.ts
+++ libs/shared/react/hooks/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/shared-react-hooks'
 export * from './lib/use-is-mounted/useIsMounted'
 export * from './lib/use-merged-ref/useMergedRef'
+export * from './lib/use-scrollbar-width/useScrollbarWidth'
