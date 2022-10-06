1af7fb74727c48781d0a4cc6e06c024c08e2d83e
Kevin Firko <kfirko@bitcurve.com>
2022-10-06 14:43:46 -0400

    ui cleanup deprecated pages + crud pages that preexisted the more dynamic ui

--- apps/ui/src/pages/app/index.tsx
+++ apps/ui/src/pages/app/index.tsx
@@ -5,21 +5,21 @@ import { BsJoystick } from 'react-icons/bs'
 
 import { PageHeading } from '../../components/elements/headings/PageHeading'
 import { useBoxProfilesQuery } from '../../api/hooks/box-profiles'
-import { useBoxContext } from '../../context/BoxContextProvider'
 import { Spinner } from '../../components/elements/feedback/Spinner'
-import type { BoxProfile } from '../../types/box-profiles.types'
 import { Heading } from '../../components/elements/headings/Heading'
 import { NavLink } from '../../components/elements/inputs/NavLink'
+// import { useBoxContext } from '../../context/BoxContextProvider'
+// import type { BoxProfile } from '../../types/box-profiles.types'
 
 const OLIVIA_PARTY_BOX_URL = 'https://olivia.party/box'
 
 export const AppIndexPage: NextPage = (_props) => {
   const { data, isSuccess, isLoading, isError } = useBoxProfilesQuery()
-  const { box: _box, setBox } = useBoxContext()
 
-  const handleSelectBoxProfile = (data: BoxProfile): void => {
-    setBox(data)
-  }
+  // const { box: _box, setBox } = useBoxContext()
+  // const handleSelectBoxProfile = (data: BoxProfile): void => {
+  //   setBox(data)
+  // }
 
   return (
     <div>
@@ -31,22 +31,24 @@ export const AppIndexPage: NextPage = (_props) => {
         {isLoading && <Spinner />}
         {isSuccess && !!data?.length && (
           <>
-            <div className="border divide-slate-200 rounded-md">
-              <div className="grid grid-cols-1 divide-y divide-slate-200">
+            <div className="">
+              <div className="grid grid-cols-1">
                 {data?.map((box) => (
                   <div
                     key={box.uuid}
                     className={clsx(
-                      'flex items-center justify-center flex-wrap p-4 sm:p-6',
-                      'text-brand-primary-darkest',
+                      'flex items-center justify-center flex-wrap p-4 sm:p-6 overflow-hidden',
+                      // 'border-b border-x first:rounded-t-md first:border-t last:rounded-b-md',
+                      'border-x border-y first:rounded-t-md first:border-t first:border-b-0 last:rounded-b-md last:border-t-0 last:border-b',
+                      'text-brand-primary-darkest border-slate-200',
                     )}
-                    onClick={(): void => {
-                      handleSelectBoxProfile({
-                        name: box.name,
-                        uuid: box.uuid,
-                        urlCode: box.urlCode,
-                      })
-                    }}
+                    // onClick={(): void => {
+                    //   // handleSelectBoxProfile({
+                    //   //   name: box.name,
+                    //   //   uuid: box.uuid,
+                    //   //   urlCode: box.urlCode,
+                    //   // })
+                    // }}
                   >
                     <BsJoystick
                       className="hidden xs:block h-12 w-12 pr-4 sm:pr-6 text-brand-primary"
