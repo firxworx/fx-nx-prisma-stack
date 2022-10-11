5d90ce0705483d5708b53b9d6142cdbe03e20bdc
Kevin Firko <kfirko@bitcurve.com>
2022-10-11 18:01:27 -0400

    ui minor cleanup

--- apps/ui/src/components/layout/AuthenticatedLayout.tsx
+++ apps/ui/src/components/layout/AuthenticatedLayout.tsx
@@ -1,15 +1,12 @@
 import React, { type PropsWithChildren } from 'react'
 import clsx from 'clsx'
 
-// import { BoxContextProvider } from '../../context/BoxContextProvider'
-
 export const AuthenticatedLayout: React.FC<PropsWithChildren> = ({ children }) => {
   // example for accessing the authenticated user's name
   // const session = useAuthSession()
   // console.log(session.profile.name)
 
   return (
-    // <BoxContextProvider>
     <div className="p-0 xs:p-4 sm:p-6 bg-slate-50">
       <div
         className={clsx(
@@ -20,6 +17,5 @@ export const AuthenticatedLayout: React.FC<PropsWithChildren> = ({ children }) =
         {children}
       </div>
     </div>
-    // </BoxContextProvider>
   )
 }
